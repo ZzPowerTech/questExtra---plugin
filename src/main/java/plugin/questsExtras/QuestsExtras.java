3c888dcd399eba1647c2c944e670f32b8adfda7a
ZzPowerTech <surikengamer@gmail.com>
2026-04-04 21:47:07 -0300

    saiu do onEnable() e foi para registerQuests(), chamado pelo ServerLoadEvent

--- src/main/java/plugin/questsExtras/QuestsExtras.java
+++ src/main/java/plugin/questsExtras/QuestsExtras.java
@@ -2,6 +2,9 @@ package plugin.questsExtras;
 
 import io.github.battlepass.BattlePlugin;
 import net.advancedplugins.bp.impl.actions.ActionRegistry;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import plugin.questsExtras.quests.DragonBreathCollectQuest;
 import plugin.questsExtras.quests.GroundItemCollectQuest;
@@ -17,8 +20,17 @@ public final class QuestsExtras extends JavaPlugin {
             return;
         }
 
-        getLogger().info("BattlePass found! Registering custom quest types...");
+        getLogger().info("BattlePass found! Aguardando inicialização completa para registrar quest types...");
 
+        getServer().getPluginManager().registerEvents(new Listener() {
+            @EventHandler
+            public void onServerLoad(ServerLoadEvent event) {
+                registerQuests();
+            }
+        }, this);
+    }
+
+    private void registerQuests() {
         ActionRegistry actionRegistry = BattlePlugin.getPlugin().getActionRegistry();
 
         actionRegistry.quest(
